2b43f05bc95cee87dd1a49e629a0080a273b0082
Yalamber Limbu <r0840560@ucll.be>
2021-02-28 16:25:24 +0100

    klant en gerecht

---
+++ Lekkerbek.Web/Models/Gerecht.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lekkerbek.Web.Models
+{
+    public enum Categorie
+    {
+        voorgerechten, hoofdgerechten, dessert
+    }
+    public class Gerecht
+    {
+    }
+}
---
+++ Lekkerbek.Web/Models/Klant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lekkerbek.Web.Models
+{
+    public class Klant
+    {
+        public int Id { get; set; }
+        public string  Naam { get; set; }
+        public string adres { get; set; }
+        public DateTime Geboortedatum { get; set; }
+        public List<Gerecht> voorkeursgerechten { get; set; } = new List<Gerecht>();
+    }
+}
