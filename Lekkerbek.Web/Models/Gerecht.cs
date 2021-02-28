93f24b1ad12e540600a23a190a8f000720883c32
Kalimath <mathieu_broe@yahoo.com>
2021-02-28 16:27:49 +0100

    Klant en Gerecht aangemaakt

---
+++ Lekkerbek.Web/Models/Gerecht.cs
@@ -0,0 +1,13 @@
+﻿namespace Lekkerbek.Web.Models
+{
+    public enum Categorie
+    {
+        voorgerecht,hoofdgerecht,dessert
+    }
+    public class Gerecht
+    {
+        public string Naam { get; set; }
+        public Categorie Categorie { get; set; }
+        public double Prijs { get; set; }
+    }
+}
