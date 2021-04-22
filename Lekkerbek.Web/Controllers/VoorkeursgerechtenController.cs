41ee3c0407a8b6f749b26ea8957ea504ba92f446
Jonas Boon <boon.jonas@outlook.com>
2021-04-22 15:08:25 +0200

    Voorkeursgerechten init - View werkt => gerecht wordt naar db gestuurd maar count blijft op 0 staan.

---
+++ Lekkerbek.Web/Controllers/VoorkeursgerechtenController.cs
@@ -0,0 +1,121 @@
+﻿using Lekkerbek.Web.Context;
+using Lekkerbek.Web.Models;
+using Lekkerbek.Web.Models.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lekkerbek.Web.Controllers
+{
+    public class VoorkeursgerechtenController : Controller
+    {
+        private readonly IdentityContext _context;
+        private readonly RoleManager<Role> _roleManager;
+        private readonly UserManager<Gebruiker> _userManager;
+
+        public VoorkeursgerechtenController(IdentityContext context, RoleManager<Role> rolemanager, UserManager<Gebruiker> userManager)
+        {
+            _context = context;
+            _roleManager = rolemanager;
+            _userManager = userManager;
+        }
+        // GET: VoorkeursgerechtenController
+        public async Task<ActionResult> Index()
+        {
+           
+            if (User.IsInRole(RollenEnum.Klant.ToString()))
+            {
+                var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+                ViewBag.User = currentUser.Id;
+
+                return View(_context.VoorkeursGerechtenVanKlanten(currentUser.Id));
+            }
+            else
+            {
+                return View(); 
+            }
+        }
+        // GET: VoorkeursgerechtenController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VoorkeursgerechtenController/Create
+        public ActionResult Create()
+        {
+
+            ViewData["GerechteId"] = new SelectList(_context.Gerechten.ToList(), "Naam", "Naam"); 
+            return View();
+        }
+
+        // POST: VoorkeursgerechtenController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(IFormCollection collection)
+        {
+            try
+            {
+                Gerecht gerecht = new Gerecht(); 
+                var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+                var gerechtvandb = await _context.Gerechten.FirstAsync(g => g.Naam.Equals(collection["Naam"])); 
+                gerecht = gerechtvandb;
+                _context.Gebruikers.First(klant => klant.Id == currentUser.Id).Voorkeursgerechten.Add(gerecht);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VoorkeursgerechtenController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VoorkeursgerechtenController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VoorkeursgerechtenController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VoorkeursgerechtenController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Lekkerbek.Web/Migrations/20210419203525_Voorkeursgerechten.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lekkerbek.Web.Migrations
+{
+    public partial class Voorkeursgerechten : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Gerecht_AspNetUsers_GebruikerId",
+                table: "Gerecht");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Gerecht_GebruikerId",
+                table: "Gerecht");
+
+            migrationBuilder.DropColumn(
+                name: "GebruikerId",
+                table: "Gerecht");
+
+            migrationBuilder.CreateTable(
+                name: "GebruikerGerecht",
+                columns: table => new
+                {
+                    VoorkeursgerechtenNaam = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    VoorkeursgerechtenVanKlantenId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GebruikerGerecht", x => new { x.VoorkeursgerechtenNaam, x.VoorkeursgerechtenVanKlantenId });
+                    table.ForeignKey(
+                        name: "FK_GebruikerGerecht_AspNetUsers_VoorkeursgerechtenVanKlantenId",
+                        column: x => x.VoorkeursgerechtenVanKlantenId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GebruikerGerecht_Gerecht_VoorkeursgerechtenNaam",
+                        column: x => x.VoorkeursgerechtenNaam,
+                        principalTable: "Gerecht",
+                        principalColumn: "Naam",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GebruikerGerecht_VoorkeursgerechtenVanKlantenId",
+                table: "GebruikerGerecht",
+                column: "VoorkeursgerechtenVanKlantenId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GebruikerGerecht");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GebruikerId",
+                table: "Gerecht",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gerecht_GebruikerId",
+                table: "Gerecht",
+                column: "GebruikerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Gerecht_AspNetUsers_GebruikerId",
+                table: "Gerecht",
+                column: "GebruikerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
